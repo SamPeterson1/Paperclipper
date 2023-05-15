4eae9160e3837c07be38cff94116b8250509c33d
Sam Peterson <yoshiapolis@gmail.com>
2023-05-15 00:04:04 -0500

    update

--- bot.js
+++ bot.js
@@ -16,10 +16,18 @@
  *	along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
+const wireSafetyMargin = 10;
+const inventorySellTime = 2;
+
+const trustGoals = [6, 47, 25, 74];
+trustGoalIndex = 0;
 
 btnLowerPrice = document.getElementById("btnLowerPrice");
 btnRaisePrice = document.getElementById("btnRaisePrice");
 btnMakePaperclip = document.getElementById("btnMakePaperclip"); 
+btnMakeClipper = document.getElementById("btnMakeClipper");
+btnBuyWire = document.getElementById("btnBuyWire");
+btnExpandMarketing = document.getElementById("btnExpandMarketing");
 
 /*
 IMPORTANT EQUATIONS
@@ -34,28 +42,63 @@ function getDemand(newMargin) {
 }
 
 //newton's method
-function approximateBestDemand() {
-    a = Math.log(clipRate * Math.log(1.15) / 0.07);
-    x = (a - Math.log(a - Math.log(a))) / Math.log(1.15);
+function getBestDemand(targetSales) {
+    bestDemand = Math.pow(targetSales / 7, 1/1.15);
 
-    for (i = 0; i < 5; i ++) {
-        err = 0.07 * x * Math.pow(1.15, x) - clipRate;
-        derivative = 0.07 * Math.pow(1.15, x) * (1 + x * Math.log(1.15));
+    if (bestDemand < 100) {
+        a = Math.log(targetSales * Math.log(1.15) / 0.07);
+        bestDemand = (a - Math.log(a - Math.log(a))) / Math.log(1.15);
 
-        x -= err / derivative;
+        for (i = 0; i < 5; i ++) {
+            err = 0.07 * bestDemand * Math.pow(1.15, bestDemand) - targetSales;
+            derivative = 0.07 * Math.pow(1.15, bestDemand) * (1 + bestDemand * Math.log(1.15));
+
+            bestDemand -= err / derivative;
+        }
+
     }
 
-    return x;
+    return bestDemand;
+}
+
+function getBestMargin(targetSales) {
+    //Match sales with production
+    bestDemand = getBestDemand(targetSales);
+    bestMargin = margin * demand / bestDemand;
+
+    //ensure we don't run out of funds
+    minMargin = Math.ceil((wireCost + wireSafetyMargin) / wireSupply) / 100;
+
+    if (bestMargin < minMargin) 
+        bestMargin = minMargin;
+
+    return bestMargin;
+}
+
+function getClipperPayback() {
+    targetSales = clipRate + clipperBoost + unsoldClips / inventorySellTime;
+    clipperRevenue = (clipRate + clipperBoost) * getBestMargin(targetSales);
+
+    return clipperRevenue / clipperCost;
+}
+
+function getMarketingPayback() {
+    marketingRevenue = clipRate * (1.1 * margin);
+
+    return marketingRevenue / adCost;
+}
+
+function buyProjects() {
+    for (i = 1; i < 219; i ++) {
+        btn = document.getElementById("projectButton" + i);
+        if (btn != null) btn.click();
+    }
 }
 
 function run() {
-    //Match sales with production
-    bestDemand = Math.pow(clipRate / 7, 1/1.15);
+    buyProjects();
 
-    if (bestDemand < 100)
-        bestDemand = approximateBestDemand();
-
-    bestMarginCents = Math.ceil(100 * margin * demand / bestDemand);
+    bestMarginCents = Math.ceil(getBestMargin(clipRate + unsoldClips / inventorySellTime) * 100);
     currentMarginCents = margin * 100;
 
     if (bestMarginCents > currentMarginCents) {
@@ -66,7 +109,35 @@ function run() {
             btnLowerPrice.click();
     }
 
-    btnMakePaperclip.click();
+    for (i = 0; i < 3; i ++) btnMakePaperclip.click();
+
+    if (wire <= Math.max(0, clipRate / 5))
+        btnBuyWire.click();
+
+    clipperPayback = getClipperPayback();
+    marketingPayback = getMarketingPayback();
+
+    if (clipperPayback > marketingPayback) {
+        //ensure we have enough funds to buy wire before buying clippers
+        if (funds + wire * margin - clipperCost > wireCost + wireSafetyMargin)
+            btnMakeClipper.click();
+    } else {
+        //ensure we have enough funds to buy wire before buying marketing
+        if (funds + wire * margin - adCost > wireCost + wireSafetyMargin)
+            btnExpandMarketing.click();
+    }
+
+    if (trustGoalIndex % 2 == 0) {
+        if (processors < trustGoals[trustGoalIndex])
+            btnAddProc.click();
+        else
+            trustGoalIndex ++;
+    } else {
+        if (memory < trustGoals[trustGoalIndex])
+            btnAddMem.click();
+        else 
+            trustGoalIndex ++;
+    }
 }
 
 setInterval(run, 100);
